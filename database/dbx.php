9946d18d6c8045ebc099719faa6569935ddd005b
Mercenary <oaishazher@gmail.com>
2024-09-30 21:31:14 +0530

    Initial Commit

--- database/dbx.php
+++ database/dbx.php
@@ -3,7 +3,7 @@ $hostname = "mercenary-oaishazher-a815.e.aivencloud.com";
 $database = "defaultdb";
 $port = 25845;
 $username = "avnadmin";
-$password = "AVNS_sozC-9DfK1BqqVjqT6m";
+$password = "";
 
 $con = @mysqli_connect($hostname, $username, $password, $database, $port);
 if(mysqli_connect_errno())
